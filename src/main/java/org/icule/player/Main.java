17ea6f59795c5ec259bf16f8d56b9eb76996eb4b
Serriere Guillaume <guillaume.serriere@telecomnancy.net>
2021-11-03 23:10:02 +0100

    Add the main function
    Signed-off-by: Serriere Guillaume <guillaume.serriere@telecomnancy.net>

---
+++ src/main/java/org/icule/player/Main.java
@@ -0,0 +1,34 @@
+package org.icule.player;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.icule.player.configuration.ConfigurationManager;
+import org.icule.player.gui.FXMLLoaderFactory;
+
+public class Main extends Application {
+    public static void main(final String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        ConfigurationManager configurationManager = ConfigurationManager.loadConfiguration(getParameters().getRaw().get(0));
+
+        Injector injector = Guice.createInjector(new MainModule(configurationManager));
+        FXMLLoaderFactory.parametrize(injector);
+
+        FXMLLoader loader = FXMLLoaderFactory.getLoader();
+        loader.setLocation(getClass().getResource("/org/icule/player/gui/MainFrame.fxml"));
+
+        BorderPane pane = loader.load();
+        stage.setScene(new Scene(pane));
+        stage.setTitle("Music player");
+
+        stage.show();
+    }
+}
--- src/main/java/org/icule/player/configuration/ConfigurationManagerDefinition.java
+++ src/main/java/org/icule/player/configuration/ConfigurationManagerDefinition.java
@@ -1,10 +1,19 @@
 package org.icule.player.configuration;
 
+import com.google.gson.Gson;
 import org.icule.player.annotation.MyStyle;
 import org.immutables.value.Value;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 @Value.Immutable
 @MyStyle
-public interface ConfigurationManagerDefinition {
-    String getDatabasePath();
+public abstract class ConfigurationManagerDefinition {
+    public abstract String getDatabasePath();
+
+    public static ConfigurationManager loadConfiguration(final String path) throws FileNotFoundException {
+        Gson gson = new Gson();
+        return gson.fromJson(new FileReader(path), ConfigurationManager.class);
+    }
 }
