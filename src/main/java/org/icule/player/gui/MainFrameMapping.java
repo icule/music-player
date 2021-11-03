d6aac3345e5cf3bf050f877d50334424c976f287
Serriere Guillaume <guillaume.serriere@telecomnancy.net>
2021-11-03 22:58:08 +0100

    Import basic structure of main frame
    Signed-off-by: Serriere Guillaume <guillaume.serriere@telecomnancy.net>

---
+++ src/main/java/org/icule/player/gui/MainFrameMapping.java
@@ -0,0 +1,51 @@
+package org.icule.player.gui;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import org.icule.player.model.Tag;
+
+import javax.inject.Inject;
+
+public class MainFrameMapping {
+    @FXML
+    private ComboBox<Tag> tagCombo;
+
+    @FXML
+    private TextArea musicInformationArea;
+
+    @Inject
+    public MainFrameMapping() {
+    }
+
+    @FXML
+    private void initialize() {
+        tagCombo.setItems(FXCollections.observableArrayList(Tag.values()));
+    }
+
+    @FXML
+    public void infoButtonAction() {
+    }
+
+    @FXML
+    public void nextButtonAction() {
+    }
+
+    @FXML
+    public void playButtonAction() {
+    }
+
+    @FXML
+    public void pauseButtonAction() {
+    }
+
+    @FXML
+    public void stopActionButton() {
+    }
+
+    @FXML
+    public void tagButtonAction() {
+    }
+
+}
---
+++ src/main/java/org/icule/player/model/Tag.java
@@ -0,0 +1,9 @@
+package org.icule.player.model;
+
+/**
+ * Created by icule on 17/07/17.
+ */
+public enum Tag {
+    GOOD,
+    TO_REMOVE
+}
