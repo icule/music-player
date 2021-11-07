5c95f98aa770fdb31818dadecf4fb5e103e5d85a
Serriere Guillaume <guillaume.serriere@telecomnancy.net>
2021-11-07 16:07:58 +0100

    Import MediaPlayerEventListenerAdapter
    Signed-off-by: Serriere Guillaume <guillaume.serriere@telecomnancy.net>

---
+++ src/main/java/org/icule/player/music/MediaPlayerEventListenerAdapter.java
@@ -0,0 +1,148 @@
+package org.icule.player.music;
+
+import uk.co.caprica.vlcj.media.MediaRef;
+import uk.co.caprica.vlcj.media.TrackType;
+import uk.co.caprica.vlcj.player.base.MediaPlayer;
+import uk.co.caprica.vlcj.player.base.MediaPlayerEventListener;
+
+public class MediaPlayerEventListenerAdapter implements MediaPlayerEventListener {
+    @Override
+    public void mediaChanged(MediaPlayer mediaPlayer, MediaRef mediaRef) {
+
+    }
+
+    @Override
+    public void opening(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void buffering(MediaPlayer mediaPlayer, float v) {
+
+    }
+
+    @Override
+    public void playing(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void paused(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void stopped(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void forward(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void backward(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void finished(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void timeChanged(MediaPlayer mediaPlayer, long l) {
+
+    }
+
+    @Override
+    public void positionChanged(MediaPlayer mediaPlayer, float v) {
+
+    }
+
+    @Override
+    public void seekableChanged(MediaPlayer mediaPlayer, int i) {
+
+    }
+
+    @Override
+    public void pausableChanged(MediaPlayer mediaPlayer, int i) {
+
+    }
+
+    @Override
+    public void titleChanged(MediaPlayer mediaPlayer, int i) {
+
+    }
+
+    @Override
+    public void snapshotTaken(MediaPlayer mediaPlayer, String s) {
+
+    }
+
+    @Override
+    public void lengthChanged(MediaPlayer mediaPlayer, long l) {
+
+    }
+
+    @Override
+    public void videoOutput(MediaPlayer mediaPlayer, int i) {
+
+    }
+
+    @Override
+    public void scrambledChanged(MediaPlayer mediaPlayer, int i) {
+
+    }
+
+    @Override
+    public void elementaryStreamAdded(MediaPlayer mediaPlayer, TrackType trackType, int i) {
+
+    }
+
+    @Override
+    public void elementaryStreamDeleted(MediaPlayer mediaPlayer, TrackType trackType, int i) {
+
+    }
+
+    @Override
+    public void elementaryStreamSelected(MediaPlayer mediaPlayer, TrackType trackType, int i) {
+
+    }
+
+    @Override
+    public void corked(MediaPlayer mediaPlayer, boolean b) {
+
+    }
+
+    @Override
+    public void muted(MediaPlayer mediaPlayer, boolean b) {
+
+    }
+
+    @Override
+    public void volumeChanged(MediaPlayer mediaPlayer, float v) {
+
+    }
+
+    @Override
+    public void audioDeviceChanged(MediaPlayer mediaPlayer, String s) {
+
+    }
+
+    @Override
+    public void chapterChanged(MediaPlayer mediaPlayer, int i) {
+
+    }
+
+    @Override
+    public void error(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void mediaPlayerReady(MediaPlayer mediaPlayer) {
+
+    }
+}
