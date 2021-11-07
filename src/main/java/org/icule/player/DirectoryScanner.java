d7146ecfb2723478effc55b4e5d90961a3daa160
Serriere Guillaume <guillaume.serriere@telecomnancy.net>
2021-11-07 22:43:20 +0100

    Add the basis of directory scanning
    Signed-off-by: Serriere Guillaume <guillaume.serriere@telecomnancy.net>

---
+++ src/main/java/org/icule/player/DirectoryScanner.java
@@ -0,0 +1,61 @@
+package org.icule.player;
+
+import org.icule.player.database.DatabaseException;
+import org.icule.player.database.DatabaseManager;
+import org.icule.player.model.Music;
+import org.icule.player.model.Playlist;
+import org.icule.player.music.MusicUtils;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class DirectoryScanner {
+    private final Playlist playlist;
+    private final DatabaseManager databaseManager;
+
+    @Inject
+    public DirectoryScanner(final Playlist playlist,
+                            final DatabaseManager databaseManager) {
+        this.playlist = playlist;
+        this.databaseManager = databaseManager;
+    }
+
+    public void scanDirectory(final File file) {
+        File[] fileList = file.listFiles();
+        if (fileList == null) {
+            return;
+        }
+
+        for (File f : fileList) {
+            if (f.isDirectory()) {
+                scanDirectory(f);
+            }
+            try {
+                String mimeType = Files.probeContentType(f.toPath());
+                if (mimeType.contains("audio")) {
+                    treatMusic(file);
+                }
+            }
+            catch (IOException e) {
+                System.out.println("Impossible to get the mimeType of file " + f.getAbsolutePath());
+            }
+            catch (DatabaseException e) {
+                System.out.println("Impossible to add the music in database");
+            }
+        }
+    }
+
+    private void treatMusic(final File file) throws IOException, DatabaseException {
+        Music toAdd = MusicUtils.getMusicFromFile(file);
+        Music inDatabase = databaseManager.getMusic(toAdd.getId());
+        if (inDatabase == null) {
+            databaseManager.addMusic(toAdd);
+            playlist.addMusic(toAdd);
+        }
+        else {
+            databaseManager.updateMusic(toAdd.withRating(inDatabase.getRating()));
+        }
+    }
+}
---
+++ src/main/java/org/icule/player/music/MusicUtils.java
@@ -0,0 +1,62 @@
+package org.icule.player.music;
+
+import org.icule.player.model.Music;
+import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
+import uk.co.caprica.vlcj.media.*;
+
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+
+public class MusicUtils {
+    private static final MediaPlayerFactory factory = new MediaPlayerFactory();
+
+    private static Media getParsedMedia(final MediaPlayerFactory factory, final String mrl) {
+        final Media media = factory.media().newMedia(mrl);
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        MediaEventListener listener = new MediaEventAdapter() {
+            @Override
+            public void mediaParsedChanged(Media media, MediaParsedStatus newStatus) {
+                latch.countDown();
+            }
+        };
+
+        try {
+            media.events().addMediaEventListener(listener);
+            if (media.parsing().parse()) {
+                latch.await();
+                return media;
+            } else {
+                return null;
+            }
+        }
+        catch (Exception e) {
+            return null;
+        }
+        finally {
+            media.events().removeMediaEventListener(listener);
+        }
+    }
+
+    private static UUID getId(final String path) {
+        Media media = getParsedMedia(factory, path);
+        try {
+            return UUID.fromString(media.meta().get(Meta.ENCODED_BY));
+        }
+        catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static Music getMusicFromFile(final File file) throws IOException {
+        UUID id = getId(file.getAbsolutePath());
+        if (id == null) {
+            throw new IOException("The audio file is not correctly tagged.");
+        }
+        return new Music(id, file.getAbsolutePath(), 3, file.lastModified());
+    }
+}
