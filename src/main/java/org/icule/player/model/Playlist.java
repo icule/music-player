f93e68276971f16935233baf0c71900e37c80b27
Serriere Guillaume <guillaume.serriere@telecomnancy.net>
2021-11-07 22:44:03 +0100

    Add UUID instead of Music in playlist interface
    Signed-off-by: Serriere Guillaume <guillaume.serriere@telecomnancy.net>

--- src/main/java/org/icule/player/model/Playlist.java
+++ src/main/java/org/icule/player/model/Playlist.java
@@ -3,9 +3,10 @@ package org.icule.player.model;
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Playlist {
-    private final List<Music> musicList;
+    private final List<UUID> musicList;
 
     @Inject
     public Playlist() {
@@ -16,13 +17,13 @@ public class Playlist {
         return musicList.size();
     }
 
-    public Music getMusic(int index) {
+    public UUID getMusic(int index) {
         return musicList.get(index);
     }
 
     public void addMusic(final Music music) {
         for (int i = 0; i < music.getRating(); ++i) {
-            musicList.add(music);
+            musicList.add(music.getId());
         }
     }
 }
