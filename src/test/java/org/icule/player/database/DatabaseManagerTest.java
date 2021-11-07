5eb5e4a44b15a2bd31be0c533685c45892a74573
Serriere Guillaume <guillaume.serriere@telecomnancy.net>
2021-11-07 16:01:37 +0100

    Add test for tag table
    Signed-off-by: Serriere Guillaume <guillaume.serriere@telecomnancy.net>

--- src/test/java/org/icule/player/database/DatabaseManagerTest.java
+++ src/test/java/org/icule/player/database/DatabaseManagerTest.java
@@ -5,6 +5,8 @@ import org.hamcrest.Matchers;
 import org.icule.player.TestUtils;
 import org.icule.player.configuration.ConfigurationManager;
 import org.icule.player.model.Music;
+import org.icule.player.model.Tag;
+import org.icule.player.model.TagMusicInformation;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,6 +24,10 @@ public class DatabaseManagerTest {
     private Music music1;
     private Music music2;
 
+    private TagMusicInformation info1;
+    private TagMusicInformation info2;
+    private TagMusicInformation info3;
+
     @Before
     public void setUp() throws FileNotFoundException, SQLException, ClassNotFoundException, DatabaseException {
         ConfigurationManager configurationManager = ConfigurationManager.loadConfiguration("configuration.json");
@@ -32,6 +38,10 @@ public class DatabaseManagerTest {
 
         music1 = new Music(UUID.randomUUID(), "test", 3, Instant.now().minusSeconds(10).toEpochMilli());
         music2 = new Music(UUID.randomUUID(), "test2", 1, Instant.now().toEpochMilli());
+
+        info1 = new TagMusicInformation(music1.getId(), Tag.GOOD, music1.getLastModification());
+        info2 = new TagMusicInformation(music1.getId(), Tag.TO_REMOVE, music1.getLastModification());
+        info3 = new TagMusicInformation(music2.getId(), Tag.GOOD, music2.getLastModification());
     }
 
     private void deleteDatabase() {
@@ -95,5 +105,62 @@ public class DatabaseManagerTest {
         assertEquals(music1, databaseManager.getMusic(music1.getId()));
         assertEquals(music2, databaseManager.getMusic(music2.getId()));
         assertThat(databaseManager.getAllMusic(), Matchers.containsInAnyOrder(music1, music2));
+    }
+
+    @Test
+    public void testAddTag() throws DatabaseException {
+
+
+        TestUtils.assertException(() -> databaseManager.addTagMusicInformation(info1), DatabaseException.class);
+        databaseManager.addMusic(music1);
+        databaseManager.addMusic(music2);
+        databaseManager.addTagMusicInformation(info1);
+        assertThat(databaseManager.getAllTagForMusic(music1.getId()), Matchers.is(Collections.singletonList(info1)));
+        assertThat(databaseManager.getAllMusicIdForTag(info1.getTag()), Matchers.is(Collections.singletonList(music1.getId())));
+
+        TestUtils.assertException(() -> databaseManager.addTagMusicInformation(new TagMusicInformation(music1.getId(), Tag.GOOD, Instant.now().toEpochMilli())),
+                                  DatabaseException.class);
+        databaseManager.addTagMusicInformation(info2);
+
+        assertThat(databaseManager.getAllTagForMusic(music1.getId()), Matchers.containsInAnyOrder(info1, info2));
+        databaseManager.addTagMusicInformation(info3);
+
+        assertThat(databaseManager.getAllMusicIdForTag(Tag.GOOD), Matchers.containsInAnyOrder(music1.getId(), music2.getId()));
+    }
+
+    @Test
+    public void testModifyTag() throws DatabaseException {
+        databaseManager.addMusic(music1);
+        databaseManager.addTagMusicInformation(info1);
+        databaseManager.addTagMusicInformation(info2);
+
+        assertThat(databaseManager.getAllTagForMusic(music1.getId()), Matchers.containsInAnyOrder(info1, info2));
+
+        TagMusicInformation updated = info1.withLastModification(Instant.now().plusSeconds(45).toEpochMilli());
+        databaseManager.updateLastModificationTime(updated);
+        assertThat(databaseManager.getAllTagForMusic(music1.getId()), Matchers.containsInAnyOrder(updated, info2));
+    }
+
+    @Test
+    public void testDeleteTag() throws DatabaseException {
+        databaseManager.addMusic(music1);
+        databaseManager.addTagMusicInformation(info1);
+        databaseManager.addTagMusicInformation(info2);
+
+        assertThat(databaseManager.getAllTagForMusic(music1.getId()), Matchers.containsInAnyOrder(info1, info2));
+        databaseManager.deleteTagMusicInformation(info1);
+        assertThat(databaseManager.getAllTagForMusic(music1.getId()), Matchers.is(Collections.singletonList(info2)));
+    }
+
+    @Test
+    public void testDeleteMusicDeleteTag() throws DatabaseException {
+        databaseManager.addMusic(music1);
+        databaseManager.addTagMusicInformation(info1);
+        databaseManager.addTagMusicInformation(info2);
+
+        assertThat(databaseManager.getAllMusicIdForTag(Tag.GOOD), Matchers.containsInAnyOrder(music1.getId()));
+        databaseManager.deleteMusic(music1.getId());
+
+        assertTrue(databaseManager.getAllMusicIdForTag(Tag.GOOD).isEmpty());
     }
 }
